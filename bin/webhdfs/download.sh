541a5e95d732c565251bddef4f4cb721bf772be7
Hong Wang <hongwang@hcdlearning.com>
2017-04-17 14:18:16 +0800

    add download script

--- bin/webhdfs/download.sh
+++ bin/webhdfs/download.sh
@@ -2,6 +2,16 @@
 
 NAMENODE=$1
 FILE_PATH=$2
+SAVE_TO=$3
 
 STORE_URL=$(curl -i -s -X GET "http://${NAMENODE}/webhdfs/v1${FILE_PATH}?op=OPEN" 2>/dev/null | grep Location | awk '{print $NF}' | sed 's/[[:cntrl:]]//')
-curl -X GET ${STORE_URL}
+
+if [ -d "${SAVE_TO}" ] ; then
+    FILE_NAME=${STORE_URL%%\?*}
+    FILE_NAME=${FILE_NAME##*/}
+    SAVE_TO="${SAVE_TO}/${FILE_NAME}"
+fi
+
+wget -q "${STORE_URL}" -O ${SAVE_TO}
+
+echo ${SAVE_TO}
